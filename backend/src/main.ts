59fb35541129c6b1d25d3551b95ec94574c7f807
Brett Corbett <137253504+bcorbett503@users.noreply.github.com>
2025-08-08 08:50:00 -0700

    Create main.ts

---
+++ backend/src/main.ts
@@ -0,0 +1,12 @@
+import 'reflect-metadata';
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { Logger } from 'nestjs-pino';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule, { bufferLogs: true });
+  app.useLogger(app.get(Logger));
+  const port = process.env.PORT ? Number(process.env.PORT) : 3000;
+  await app.listen(port);
+}
+bootstrap();
