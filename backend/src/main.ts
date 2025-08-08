c0387159725b484bea15ceccaacbdb664bb4ca92
Brett Corbett <137253504+bcorbett503@users.noreply.github.com>
2025-08-08 16:12:46 -0700

    Update main.ts

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -6,8 +6,7 @@ import { Logger } from 'nestjs-pino';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { bufferLogs: true });
   app.useLogger(app.get(Logger));
-  const port = Number(process.env.PORT ?? 3000);
-  // IMPORTANT: bind to 0.0.0.0 for Fargate/ALB to reach it
-  await app.listen(port, '0.0.0.0');
+  const port = Number(process.env.PORT || 3000);
+  await app.listen(port, '0.0.0.0'); // <-- important for ECS/ALB
 }
 bootstrap();
